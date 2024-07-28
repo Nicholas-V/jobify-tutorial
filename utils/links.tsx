8135bae81bf94b60e3d8a458e70f72ef5c44ae7b
Nicholas-V <nickvasylyniuk@gmail.com>
2024-07-28 17:09:05 -0400

    fixing pagination

--- utils/links.tsx
+++ utils/links.tsx
@@ -1,27 +1,27 @@
-import {AreaChart, Layers, AppWindow} from "lucide-react";
+import { AreaChart, Layers, AppWindow } from 'lucide-react';
 
 type NavLink = {
-    href: string;
-    label: string;
-    icon: React.ReactNode;
+  href: string;
+  label: string;
+  icon: React.ReactNode;
 };
 
-
 const links: NavLink[] = [
-    {
-        href: "/add-job",
-        label: "add job",
-        icon: <Layers/>
-    },
-    {
-        href: "/jobs",
-        label: "all jobs",
-        icon: <AppWindow/>
-    },
-    {
-        href: "/stats",
-        label: "stats",
-        icon: <AreaChart/>
-    },
-]
+  {
+    href: '/add-job',
+    label: 'add job',
+    icon: <Layers />,
+  },
+  {
+    href: '/jobs',
+    label: 'all jobs',
+    icon: <AppWindow />,
+  },
+  {
+    href: '/stats',
+    label: 'all stats',
+    icon: <AreaChart />,
+  },
+];
+
 export default links;
