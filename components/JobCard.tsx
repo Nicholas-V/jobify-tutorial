8173a2e6dd0406790cb19b30890d6d5830394270
Nicholas-V <nickvasylyniuk@gmail.com>
2024-07-28 14:34:31 -0400

    continue working on job cards

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -17,12 +17,20 @@ import JobInfo from './JobInfo';
 import DeleteJobButton from './DeleteJobButton';
 
 function JobCard({job}: { job: JobType }) {
+    const date = new Date(job.createdAt).toLocaleDateString();
     return <Card className="bg-muted ">
         <CardHeader>
             <CardTitle>{job.position}</CardTitle>
             <CardDescription>{job.company}</CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent className="mt-4 grid grid-cols-2 gap-4">
+            <JobInfo icon={<Briefcase/>} text={job.mode}/>
+            <JobInfo icon={<MapPin/>} text={job.location}/>
+            <JobInfo icon={<CalendarDays/>} text={date}/>
+            <Badge className="w-32 justify-center">
+                <JobInfo icon={<RadioTower className="w-4 h-4"/>} text={job.status}/>
+            </Badge>
+        </CardContent>
         <CardFooter className="flex gap-4">
             <Button asChild size="sm">
                 <Link href={`/jobs/${job.id}`}>edit</Link>
